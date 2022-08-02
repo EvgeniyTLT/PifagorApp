f9504d1d13b23a512c79c53150d2d058cec9be74
EvgeniyTLT <Toy444@ya.ru>
2022-08-03 00:37:20 +0300

    add

--- app/src/main/java/com/example/pifagorapp/MainActivity.kt
+++ app/src/main/java/com/example/pifagorapp/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.pifagorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.pifagorapp.databinding.ActivityMainBinding
+import java.lang.Math.sqrt
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    fun onClickResult(view: View) {
+        if (!isFieldEmpty()) {
+            val result = getString(R.string.result_info) + getResult()
+            binding.tvResult.text = result
+        }
+    }
+
+    private fun isFieldEmpty(): Boolean {
+        binding.apply {
+            if (edA.text.isNullOrEmpty()) edA.error = "Поле должно быть заполнено"
+            if (edB.text.isNullOrEmpty()) edB.error = "Поле должно быть заполнено"
+            return edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+        }
+    }
+
+    private fun getResult(): String {
+        val a: Double
+        val b: Double
+        val c: Double
+        binding.apply {
+            a = edA.text.toString().toDouble()
+            b = edB.text.toString().toDouble()
+        }
+        return sqrt((a.pow(2) + b.pow(2))).toString()
     }
 }
